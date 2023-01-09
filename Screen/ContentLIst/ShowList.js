52a1cb3f762693318b49260bd3d747453d0bed98
Limsohee <v_vsoi@naver.com>
2023-01-10 01:33:35 +0900

    [UPDATE] [subject:main] 미술관 아이콘 생성 [해결]

---
+++ Screen/ContentLIst/ShowList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShowList
--- Screen/Main/Main.js
+++ Screen/Main/Main.js
@@ -1,27 +1,46 @@
-import * as React from 'react';
-import { StyleSheet,View, Text, Button, Image } from "react-native";
-import styled, {css} from 'styled-components/native';
-import Theme from '../../Theme/Theme';
+import * as React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import styled, { css } from "styled-components/native";
+import Theme from "../../Theme/Theme";
+import gallery from "../../assets/Icon/gallery.svg";
 
 const Main = ({ navigation }) => {
-  
   return (
     <StView style={styles.container}>
-    <Text>메인화면</Text>
-    <GalleryView
-      onPress={() => navigation.navigate("FineArtList")}
-    ></GalleryView>
-  </StView>
-  )
-}
+      <Text>메인화면</Text>
+      <TouchableOpacity
+        style={styles.center}
+        onPress={() => navigation.navigate("FineArtList")}
+      >
+        <Image source={require("../../assets/Icon/gallery.png")} />
+        <Text>미술관</Text>
+      </TouchableOpacity>
+    </StView>
+  );
+};
 
-export default Main
-
+export default Main;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  smallRound: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  center: {
     alignItems: "center",
     justifyContent: "center",
   },
@@ -31,11 +50,8 @@ const StView = styled.View`
   background-color: pink;
 `;
 
-const GalleryView = styled.View`
+const SmallRound = styled.View`
   width: 50px;
   height: 50px;
-  border-radius: 50%;
   border: 1px solid ${Theme.colors.LightGray};
 `;
-
-
