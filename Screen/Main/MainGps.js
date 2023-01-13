18828524dc5897cf1ec453c6a7a3e52389d9206b
Limsohee <v_vsoi@naver.com>
2023-01-14 02:15:33 +0900

    [UPDATE] [subject:main] gps위치 가져오기 hook 생성 및 적용 [해결]

--- Screen/Main/MainGps.js
+++ Screen/Main/MainGps.js
@@ -4,42 +4,65 @@ import { Text, View, StyleSheet, ScrollView, Dimensions } from "react-native";
 import styled, { css } from "styled-components/native";
 import Theme from "../../Theme/Theme";
 import useGpsAsk from "../../utils/useGpsAsk";
+import useGpsRes from "../../utils/useGpsRes";
 
 export default function MainGps() {
-  const [region, setRegion] = useState("Loading...");
-  const [district, setDistrict] = useState();
-  const [location, setLocation] = useState();
-  const [ok, setOk] = useState(true);
-  const ask = async () => {
-      const granted = useGpsAsk();
-      if(granted){
-        const {
-            coords: { latitude, longitude },
-          } = await Location?.getCurrentPositionAsync({ accuracy: 5 });
-          const location = await Location?.reverseGeocodeAsync(
-            { latitude, longitude },
-            { useGoogleMaps: false }
-          );
-          setRegion(location[0]?.region);
-          setDistrict(location[0]?.district);
-          console.log(location);
-      } else {
-        setOk(false);
-      } 
-    
-  };
-
-  useEffect(() => {
-    ask();
+  const [gpsRes,setGpsRes]=useState({region:"Loading...",district:""})
+  const gps = useGpsRes();
+  console.log('gps===>',gps);
+    useEffect(async() => {
+    const {region,district} = await gps;
+    console.log("region,district===>",region,district)    
+    setGpsRes({...gpsRes,region:region, district:district});
+    // useGpsRes();
   }, []);
-
+  console.log("gpsRes===>",gpsRes)
   return (
     <UserGps>
-      {region} {district}
+      {gpsRes?.region} {gpsRes?.district}
     </UserGps>
   );
 }
 const UserGps = styled.Text`
-  color: ${Theme.colors.SkyBlue}; 
+  color: ${Theme.colors.SkyBlue};
   font-weight: 700;
 `;
+
+// export default function MainGps() {
+//   const [region, setRegion] = useState("Loading...");
+//   const [district, setDistrict] = useState();
+//   const [location, setLocation] = useState();
+//   const [ok, setOk] = useState(true);
+//   const place = async () => {
+//       const granted = useGpsAsk(); 
+//       console.log("granted==>",granted);     
+//       if(granted){
+//         const {
+//             coords: { latitude, longitude },
+//           } = await Location?.getCurrentPositionAsync({ accuracy: 5 });
+//           const location = await Location?.reverseGeocodeAsync(
+//             { latitude, longitude },
+//             { useGoogleMaps: false }
+//           );
+//           setRegion(location[0]?.region);
+//           setDistrict(location[0]?.district);
+//       } else {
+//         setOk(false);
+//       }
+
+//   };
+
+//   useEffect(() => {
+//     place();
+//   }, []);
+
+//   return (
+//     <UserGps>
+//       {region} {district}
+//     </UserGps>
+//   );
+// }
+// const UserGps = styled.Text`
+//   color: ${Theme.colors.SkyBlue};
+//   font-weight: 700;
+// `;
