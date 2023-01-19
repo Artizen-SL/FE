89497e09a79f6b4ecc2b9e973425b0f5a5046c33
kyunghoonkook <rnrrudgg68@gmail.com>
2023-01-19 14:37:37 +0900

    no message

--- utils/useGpsRes.js
+++ utils/useGpsRes.js
@@ -1,6 +1,6 @@
 import * as Location from "expo-location";
 import { useState } from "react";
-import useGpsAsk from "./useGpsAsk";
+// import useGpsAsk from "./useGpsAsk";
 
 const useGpsRes = async () => {
   const granted = await useGpsAsk();
@@ -17,11 +17,11 @@ const useGpsRes = async () => {
     );
     region = location[0]?.region;
     district = location[0]?.district;
-    return { region, district};
+    return { region, district };
   } else {
     disagree = "Loding";
   }
-  
+
   return region, district, disagree;
 };
 
