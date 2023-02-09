c03a96ecdf7aacf69e86835f591e473c71283710
Limsohee <v_vsoi@naver.com>
2023-02-10 04:32:27 +0900

    [UPDATE] [subject:notice] 공지등록하기 ui로직 구현 [해결]

--- Components/Notice/NoticePost.js
+++ Components/Notice/NoticePost.js
@@ -1,23 +1,46 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-
-import {
-  FlatList,
-  ImageBackground,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import styled from "styled-components/native";
+import PrNoticePost from "./Presenters/PrNoticePost";
+import * as ImagePicker from 'expo-image-picker';
+import { useState, useEffect } from "react";
 
 const NoticePost = () => {
-  return(
-    <View>
-        <Text>공지등록하기</Text>
-    </View>
-  ) 
+  const [image, setImage] = useState(null);
+  const [important,setImportant] = useState(false);
+  const init = {
+    title:"",
+    content:"",
+  }
+  const [notice,setNotice] = useState(init);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      // allowsEditing: true,
+      // aspect: [4, 3],
+      allowsMultipleSelection: true,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const onChangeText = (e) => {
+
+  }
+  
+
+
+  return<PrNoticePost 
+  setImportant={setImportant} 
+  important={important}
+  notice={notice}
+  pickImage={pickImage}
+  image={image}
+  />
 }
 
 export default NoticePost
