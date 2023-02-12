493e04cba7f37dc1358256432b69e110f24140b0
Limsohee <v_vsoi@naver.com>
2023-02-13 03:43:56 +0900

    [UPDATE] [subject:my pages]  마이티켓 겟요청 기능구현 [해결]

--- Components/MyPage/Presenters/PrMyTicket.js
+++ Components/MyPage/Presenters/PrMyTicket.js
@@ -1,35 +1,74 @@
 import {
-    StyleSheet,
-    View,
-    Text,
-    Button,
-    Image,
-    TextInput,
-    TouchableOpacity,
-    ImageBackground,
-    Dimensions,
-  } from "react-native";
-  import styled, { css } from "styled-components/native";
-  import Theme from "../../../Theme/Theme";
-  import ScrollViewLayout from "../../Layout/ScrollViewLayout";
-  import { SafeAreaView } from "react-native-safe-area-context";
-  import Layout from "../../Layout/Layout";
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Dimensions,
+  FlatList,
+} from "react-native";
+import styled, { css } from "styled-components/native";
+import Theme from "../../../Theme/Theme";
+import ScrollViewLayout from "../../Layout/ScrollViewLayout";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Layout from "../../Layout/Layout";
+import PrMyPageWrapper from "./PrMyPageWrapper";
 
-const PrMyTicket = ({navigation}) => {
+const PrMyTicket = ({ navigation, myTicketDatas, loadMore }) => {
+  const renderItem = ({ item }) => {
+    console.log( " item ",item )
     return (
-        <ScrollViewLayout>
-            <TouchableOpacity
-             onPress={()=>{
-                navigation.navigate("MyPageRoutes", {
-                    screen: "MyTicketPost",
-                  })
-              }}
-            >
-                <Text>글쓰기</Text>
-            </TouchableOpacity>
-            <Text> 마이티켓</Text>
-        </ScrollViewLayout>
-      )
-}
+      <Box>
+        <ImageBox source={{ uri: item.ticketImg}} />
+        <Text>{item.cultureName}</Text>
+      </Box>
+    );
+  };
+  return (
+    <ScrollViewLayout style={{ flex: 1 }}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("MyPageRoutes", {
+            screen: "MyTicketPost",
+          });
+        }}
+      >
+        <Text>글쓰기</Text>
+      </TouchableOpacity>
+      {/* {myTicketDatas && myTicketDatas.length > 0 ? ( */}
+        <FlatList
+          // ListHeaderComponent={<></>}
+          renderItem={renderItem}
+          data={myTicketDatas}
+          keyExtractor={(item) => item?.id}
+          onEndReached={loadMore}
+          onEndReachedThreshold={0.5}
+          // contentContainerStyle={{}}
+          // ListFooterComponent={}
+        />
+      {/* ) : (
+        <Layout>
+          <Text>내가 쓴 글이 없어요</Text>
+        </Layout>
+      )} */}
+    </ScrollViewLayout>
+  );
+};
 
-export default PrMyTicket
+export default PrMyTicket;
+
+const Box = styled(View)`
+  width: 40%;
+  height: 217px;
+  background-color: ${Theme.colors.White};
+  border-radius: 5px;
+  border: 1px solid ${Theme.colors.LightGray};
+`;
+
+const ImageBox = styled(Image)`
+  width: 40%;
+  height: 139px;
+`;
