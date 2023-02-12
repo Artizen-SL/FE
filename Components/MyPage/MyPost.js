fd0a104d90e549f1a9a21c3cd1a0fdbc42629df8
Limsohee <v_vsoi@naver.com>
2023-02-12 20:17:01 +0900

    [FEAT] [subject:mypage] mypage 컴포넌트생성, 라우트 구현 [해결]

---
+++ Components/MyPage/MyPost.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import PrMyPost from './Presenters/PrMyPost'
+const MyPost = () => {
+  return<PrMyPost/>
+}
+
+export default MyPost
---
+++ Components/MyPage/Presenters/PrMyTicketPost.js
@@ -0,0 +1,26 @@
+import {
+    StyleSheet,
+    View,
+    Text,
+    Button,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    ImageBackground,
+    Dimensions,
+  } from "react-native";
+  import styled, { css } from "styled-components/native";
+  import Theme from "../../../Theme/Theme";
+  import ScrollViewLayout from "../../Layout/ScrollViewLayout";
+  import { SafeAreaView } from "react-native-safe-area-context";
+  import Layout from "../../Layout/Layout";
+
+const PrMyTicketPost = () => {
+  return (
+    <View>
+        <Text> 마이티켓 글쓰기</Text>
+    </View>
+  )
+}
+
+export default PrMyTicketPost
